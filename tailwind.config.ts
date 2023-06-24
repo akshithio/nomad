0d939354ff9c9e7e9af9d0cc3516173ed01ec770
Akshith <garapatiakshith@gmail.com>
2023-06-24 11:47:23 +0530

    feat:landing + ideation

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,12 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        cal: ["Cal Sans"],
+        inter: ["Inter"],
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
