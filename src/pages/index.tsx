97d0239d5b27b12fb57f1aa3e3b3f1b158a32d8b
Akshith <garapatiakshith@gmail.com>
2023-06-24 11:58:01 +0530

    fix: auth deleted due to merge issues, fixed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -187,3 +187,27 @@ export default function Home() {
     </>
   );
 }
+
+function AuthShowcase() {
+  const { data: sessionData } = useSession();
+
+  const { data } = api.travel.getAllPlans.useQuery(
+    undefined, // no input
+    { enabled: sessionData?.user !== undefined }
+  );
+
+  return (
+    <div className="flex flex-col items-center justify-center gap-4">
+      <p className="text-center text-2xl text-white">
+        {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
+        {data && <span> - {JSON.stringify(data)}</span>}
+      </p>
+      <button
+        className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
+        onClick={sessionData ? () => void signOut() : () => void signIn()}
+      >
+        {sessionData ? "Sign out" : "Sign in"}
+      </button>
+    </div>
+  );
+}
