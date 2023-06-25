98b39970ff05e6f9a4f3111e72cff249d0dcdefa
Akshith <garapatiakshith@gmail.com>
2023-06-25 19:58:31 +0530

    feat:minor additions

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -3,6 +3,8 @@ import Link from "next/link";
 import * as Popover from "@radix-ui/react-popover";
 import { ExitIcon, PersonIcon } from "@radix-ui/react-icons";
 import { signIn, signOut, useSession } from "next-auth/react";
+import * as Dialog from "@radix-ui/react-dialog";
+import { Cross2Icon } from "@radix-ui/react-icons";
 
 // @ts-expect-error anything
 export default function Navbar(props: any) {
@@ -49,10 +51,45 @@ export default function Navbar(props: any) {
                   <h1>Logout</h1>
                   <ExitIcon className="absolute right-10" />
                 </button>
-                <button className="relative flex w-full items-center p-3">
-                  <h1>Edit Profile</h1>
-                  <PersonIcon className="absolute right-10" />
-                </button>
+                <Dialog.Root>
+                  <Dialog.Trigger className="w-full">
+                    <button className="relative flex w-full items-center p-3">
+                      <h1>Profile Details</h1>
+                      <PersonIcon className="absolute right-10" />
+                    </button>
+                  </Dialog.Trigger>
+                  <Dialog.Portal>
+                    <Dialog.Overlay className="DialogOverlay rounded-md" />
+                    <Dialog.Content className="DialogContent">
+                      <Dialog.Title className="DialogTitle font-cal text-2xl">
+                        User Details
+                      </Dialog.Title>
+                      <Dialog.Description className="DialogDescription">
+                        All of the details regarding you that help us keep track
+                        of all the journeys you embark on
+                      </Dialog.Description>
+
+                      <h1 className="mt-2">
+                        <span className="font-cal">Email ID: </span>
+                        {sessionData?.user?.email}
+                      </h1>
+                      <h1 className="mt-2">
+                        <span className="font-cal">Username: </span>
+                        {sessionData?.user?.name}
+                      </h1>
+                      <h1 className="mt-2">
+                        <span className="font-cal">User ID: </span>
+                        {sessionData?.user?.id}
+                      </h1>
+
+                      <Dialog.Close asChild>
+                        <button className="IconButton" aria-label="Close">
+                          <Cross2Icon />
+                        </button>
+                      </Dialog.Close>
+                    </Dialog.Content>
+                  </Dialog.Portal>
+                </Dialog.Root>
               </div>
             </Popover.Content>
           </Popover.Portal>
